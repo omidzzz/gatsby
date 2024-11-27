2ecc88dff7adf2c129dc336e506e7548d3ba89b8
Sinisteroid <ghadamgahi.omid@gmail.com>
2024-11-27 13:00:28 +0330

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle={"Home"}>
       <p>This is the content of the Homepage!</p>
+      <StaticImage src='../images/Mastodon.png' alt='Picsum' />
     </Layout>
   )
 }
